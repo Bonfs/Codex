bcc5cd9715c3a6479cc2a8be054c2437fdb6d312
matheusbonfim05@outlook.com <matheusbonfim05@outlook.com>
2015-12-19 00:11:57 -0200

    Adição da função busca
    busca adicionada a busca.html|buscaCtrl.js

--- www/js/controllers/buscaCtrl.js
+++ www/js/controllers/buscaCtrl.js
@@ -1,8 +1,8 @@
 angular.module('codex').controller('buscaCtrl', ['$scope', '$ionicHistory', function ($scope, $ionicHistory){
-	$scope.teste = function ()
-	{
-		console.log($ionicHistory.viewHistory().views);
-		//console.log($ionicHistory.viewHistory().histories);
-		//$ionicHistory.backView();
-	}
+	$scope.titulo = 'Livros';
+	$scope.livros = [
+			{id: 1, nome_livro: 'Livro 1', codigo: '333f.8', autor: 'M. Bonfim', ano: 1999},
+			{id: 2, nome_livro: 'Livro 2', codigo: '443f.20', autor: 'BONFIM', ano: 2000},
+			{id: 3, nome_livro: 'Criataruas Mágicas e Onde Habitam', codigo: '520.a5', autor: 'J.K. Rowling', ano: 2005}
+		];
 }])
